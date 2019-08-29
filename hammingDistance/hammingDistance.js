362e00f83a5cc03877d76f26c0db93f1d2690d5d
Hyunsung Lim <hslgood@gmail.com>
2019-08-29 09:23:31 +0900

    hammingDistance complete

--- hammingDistance/hammingDistance.js
+++ hammingDistance/hammingDistance.js
@@ -1,6 +1,13 @@
 function hammingDistance(strArr) {
-  // YOUR CODE HERE
-  return
+  
+  let result = 0;
+
+  for ( let i=0; i<strArr[0].length; i=i+1 ) {
+    if ( strArr[0][i] !== strArr[1][i]) {
+      result = result +1 ;
+    }
+  }
+  return result
 }
 
 hammingDistance(["coder", "codec"]); // => 1
